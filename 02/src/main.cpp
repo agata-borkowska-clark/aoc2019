9c2ba3b6dd6b0368146325c9c3da620b823b6006
Agata Borkowska <borkowska.agata37@gmail.com>
2019-12-02 21:01:47 +0100

    day 2 part 2

--- 02/src/main.cpp
+++ 02/src/main.cpp
@@ -3,32 +3,45 @@
 #include <vector>
 
 int main() {
+
   std::fstream file("input.txt");
-  std::vector<int> v;
+  std::vector<int> original;
   int a;
   char comma;
   while (file >> a >> comma) {
-    v.push_back(a);
-  }
-  v.at(1) = 12;
-  v.at(2) = 2;
-  unsigned long current = 0;
-  while (current < v.size()) {
-    switch (v.at(current)) {
-      case 1:
-	v.at(v.at(current + 3)) = v.at(v.at(current + 1)) + v.at(v.at(current + 2));
-	current += 4;
-        break;
-      case 2:
-	v.at(v.at(current + 3)) = v.at(v.at(current + 1)) * v.at(v.at(current + 2));
-	current += 4;
-	break;
-      case 99:
-	printf("%d\n", v.at(0));
-	return 0;
-      default:
-	printf("Something went wrong\n");
-	return 1;
+    original.push_back(a);
+  }   
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      std::vector<int> v = original;
+      v.at(1) = i;
+      v.at(2) = j;
+      unsigned long current = 0;
+      while (current < v.size()) {
+        switch (v.at(current)) {
+          case 1:
+	    v.at(v.at(current + 3)) = v.at(v.at(current + 1)) + v.at(v.at(current + 2));
+	    current += 4;
+            break;
+          case 2:
+	    v.at(v.at(current + 3)) = v.at(v.at(current + 1)) * v.at(v.at(current + 2));
+	    current += 4;
+	    break;
+          case 99:
+	    if (v.at(0) == 19690720) {
+	      printf("%d\n", i * 100 + j);
+	      return 0;
+	    } else {
+	      current = v.size();
+	    }
+	    break;
+          default:
+	    printf("Something went wrong\n");
+	    return 1;
+
+	}
+      }
     }
   }
+  return 2;
 }
