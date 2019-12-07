b22e0b9eb278b5b58eff7415d1f7ec788136f1e2
Agata Borkowska <borkowska.agata37@gmail.com>
2019-12-07 14:11:36 +0100

    segfault fixed (day6 pt 1)

--- 06/src/main.cpp
+++ 06/src/main.cpp
@@ -17,16 +17,21 @@ int main() {
   std::map<std::string, body*> map;
   std::fstream file("input.txt");
   std::string line;
+  int line_num = 0;
   while(std::getline(file, line)) {
     std::string object_a_name = line.substr(0, 3);
     std::string object_b_name = line.substr(4, 3);
     auto object_a = map.find(object_a_name);
     if (object_a == map.end()) {
+      //std::cout << "line " << line_num << ": creating object a\n";
       auto object_a_new = std::make_pair(object_a_name, new body());
       object_a_new.second->name = object_a_name;
       map.insert(object_a_new);
     }
+    object_a = map.find(object_a_name);
+    
     if (object_a->first == "COM") {
+      std::cout << "CoM found at line " << line_num << "\n";
       com = object_a->second;
     }
     auto object_b = map.find(object_b_name);
@@ -40,10 +45,12 @@ int main() {
       object_b_new.second->parent = object_a->second;
       map.insert(object_b_new);
     }
-    map.find(object_a_name)->second->orbitters.push_back(object_b->second);
+    map.find(object_a_name)->second->orbitters.push_back(map.find(object_b_name)->second);
   }
+  std::cout << "File parsed\n";
   std::queue<body*> to_process;
-  for (auto object : com->orbitters) {
+  for (auto const object : com->orbitters) {
+    std::cout << "Orbitters of CoM\n" << object->orbit_count << '\n';
     object->orbit_count = 1;
     for (auto o : object->orbitters) {
       to_process.push(o);
