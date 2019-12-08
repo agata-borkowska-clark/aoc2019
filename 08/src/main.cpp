224bfa32c6a26c48541a98fce55be1257fddf774
Agata Borkowska <borkowska.agata37@gmail.com>
2019-12-08 14:11:35 +0100

    day 8 pt 2

--- 08/src/main.cpp
+++ 08/src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <vector>
 
 struct layer {
   std::string pixels;
@@ -9,16 +10,17 @@ struct layer {
   int ones = 0;
   int twos = 0;
 };
+//const int layer_size = 2 * 2;
 const int layer_size = 25 * 6;
 
 int main() {
   std::fstream file("input.txt");
   std::map<std::string, layer> layer_map;
+  std::vector<layer> stack;
   char pixel;
-  file >> pixel;
   std::string candidate_name;
   int candidate_count = layer_size;
-  while (file.get(pixel)) {
+  while (file >> pixel) {
     char layer_pixels[layer_size];
     int zero_count = 0;
     int one_count = 0;
@@ -32,7 +34,9 @@ int main() {
       } else {
         ++two_count;
       }
-      file >> pixel;
+      if (i != layer_size - 1) {
+        file >> pixel;
+      }
     }
     //std::cout << std::string(layer) << " " << zero_count << " " << one_count << " " << two_count << " " << ++count <<"\n";
     layer l;
@@ -41,13 +45,42 @@ int main() {
     l.ones = one_count;
     l.twos = two_count;
     layer_map.emplace(l.pixels, l);
+    stack.push_back(l);
     if (zero_count < candidate_count) {
       candidate_count = l.zeroes;
       candidate_name = l.pixels;
     }
+    //std::cout << l.pixels << "\n";
   }
   int a, b;
   a = layer_map.find(candidate_name)->second.ones;
   b = layer_map.find(candidate_name)->second.twos;
   std::cout << "Part 1: " << a * b << "\n";
+  //std::cout << "layer count " << stack.size() << "\n"; 
+  // Part 2
+  char final_picture[layer_size];
+  for (int i = 0; i < layer_size; ++i) {
+    int depth = 0;
+    char pixel = stack[depth].pixels.at(i);
+    //std::cout << pixel << "\n";
+    while (pixel == '2') {
+      //std::cout << stack[depth].pixels << "\n";
+      pixel = stack[++depth].pixels.at(i);
+      //std::cout << pixel << "\n";
+    }
+    final_picture[i] = pixel;
+  }
+  std::cout << "Part 2:\n";
+  for (int x = 0; x < 6; ++x) {
+    for (int y = 0; y < 25; ++y) {
+      char c = final_picture[x * 25 + y];
+      if (c == '0') {
+        std::cout << ' ';
+      } else {
+        std::cout << '#';
+      }
+    }
+    std::cout << "\n";
+  }
 }
+
