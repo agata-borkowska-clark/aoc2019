dd6dbf3c9013231c406da09208dbf8116002709b
Agata Borkowska <borkowska.agata37@gmail.com>
2019-12-14 17:55:08 +0100

    you can't rest now, there are enemies nearby

--- intcomputer/src/intcomputer.cpp
+++ intcomputer/src/intcomputer.cpp
@@ -20,7 +20,68 @@ public std::vector<long> intcomputer::run() {
   }
 }
 
+public std::vector<long> intcomputer::run(long  input) {
+  this.input = new std::deque<long>(1, input);
+  return run();
+}
 
+public std::vector<long> intcomputer::run(std::deque<long> input) {
+  this.input = input;
+  return run();
+}
 
+public void queue_input(long input) {
+  this.input->push_back(input);
+}
 
+private std::pair<std::string, long> split_instruction(long i) {
+  long instruction = i % 100;
+  std::string mode = "000";
+  std::string instruction_string = to_string(i);
+  int index = instruction_string.length() - 3;
+  int original_mode_length = index + 1;
+  while (index >= 0) {
+    mode[3 - original_mode_length + index] = instruction_string[index];
+    --index;
+  }
+  return std::pair<std::string, logn> (mode, instruction);
+}
 
+private char supply_mode(std::string* mode, int param_index) {
+
+}
+
+private long obtain_value(long val, char mode) {
+
+}
+
+private voide perform_instruction() {
+    // get instruction and mode
+    std::pair<std:string, long> mode_instr_pair = split_instruction(*current);
+    switch (pair->second) {
+      case 1:
+
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 99:
+        break;
+      default:
+        break;
+    }
+
+}
--- intcomputer/src/intcomputer.h
+++ intcomputer/src/intcomputer.h
@@ -9,11 +9,6 @@
 
 class intcomputer {
   public:
-    std::map<unsigned long, long> local_mem;
-    unsigned long relative_base = 0;
-    unsigned long current = 0;  // current should always point at an instruction
-    std::deque<long> input = nullptr;
-
     // create an instance of the Integer Computer with a vector as instruction stream
     intcomputer(std::vector<long> v);
     // create an instance of the Integer Computer with a map as the instruction stream
@@ -22,16 +17,28 @@ class intcomputer {
     // manually move the pointer to an instruction
     // (calls to this method should be avoided)
     void set_current(long x);
+    // change the relative base
+    // calls to this method from other classes should be avoided)
     void set_relative_base(long x);
+    // add a value to the input list
+    void queue_input(long input);
     
     // run the program
     std::vector<long> run();
-    std::vector<long> run(int input);
-    std::vector<long> run(std::vector<long> input);
+    // run the program with a single input
+    std::vector<long> run(long input);
+    // run the program with a list of input values
+    // It will replace any existing input with the new value.
+    std::vector<long> run(std::deque<long> input);
 
   private:
-    std::pair<std::string, int> split_instruction(int i);
+    std::map<unsigned long, long> local_mem;
+    unsigned long relative_base = 0;
+    unsigned long current = 0;  // current should always point at an instruction
+    std::deque<long> input;
+
+    std::pair<std::string, long> split_instruction(long i);
     char supply_mode(std::string* mode, int param_index);
-    long get_parameter(long val);
-    void perform_instruction(long);
+    long obtain_value(long val, char mode);
+    void perform_instruction();
 }
