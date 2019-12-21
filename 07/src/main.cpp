e7ad57cae83359a14b58b3f7574ac94278e4b0a9
Agata Borkowska <borkowska.agata37@gmail.com>
2019-12-21 19:30:06 +0100

    changing day7 p1 to use int computer

--- 07/src/main.cpp
+++ 07/src/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <string>
 #include <vector>
 
+#include "../../intcomputer/src/intcomputer.h"
+
+long create_and_run(long phase, long signal, const std::vector<long>& original) {
+  intcomputer amp(original);
+  amp.queue_input(phase);
+  amp.queue_input(signal);
+  std::vector<long> result = amp.run();
+  return result[0];
+};
+
 int main() {
-  int arr[5] = {0, 1, 2, 3, 4};
-  int max = 0;
+  long arr[5] = {0l, 1l, 2l, 3l, 4l};
+  long max = 0l;
   std::fstream file("input.txt");
   std::vector<long> original;
   long a;
@@ -17,124 +26,14 @@ int main() {
   file >> a;
   original.push_back(a);
   do {
-    int input = 0;
+    long result = 0;
     std::cout << "Permutation: " << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << arr[4] << "\n";
     for (int i = 0; i < 5; ++i) {
-      bool phase_setting = true;
-      std::vector<long> v = original;
-      //int input_pointer = 0;
-      //v[1] = 12;
-      //v[2] = 2;
-      unsigned long current = 0;
-      bool running = true;
-      while (running) {
-        long instruction = v[current];
-        std::string mode = "000";
-        if (instruction > 99) {
-          mode = std::to_string(instruction / 100);
-          instruction = instruction % 100;
-        }
-        if (mode.size() == 2) {
-          mode = std::string("0").append(mode);
-        } else if (mode.size() == 1) {
-          mode = std::string("00").append(mode);
-        }
-        // std::cout << "mode: " <<  mode << " instruction: " << instruction << " position: " << current <<"\n";
-        long param1 = 0;
-        long param2 = 0;
-        long param3 = 0;
-        switch (instruction) {
-          case 1:
-            // add param1 and param2
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            v[v[current + 3]] = param1 + param2;
-            current += 4;
-            break;
-          case 2:
-            // multiply param1 by param2
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            v[v[current + 3]] = param1 * param2;
-            current += 4;
-            break;
-          case 3:
-            // store input at position param1
-            if (phase_setting) {
-              v[v[current + 1]] = arr[i];
-              phase_setting = false;
-            } else {
-              v[v[current + 1]] = input;
-            }
-            current += 2;
-            break;
-          case 4:
-            //output value from param1
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            //std::cout << "Instruction 4: " << param1 << "\n";
-            input = param1;
-            current += 2;
-            break;
-          case 5:
-            //jump to param2 if param1 is non-zero
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            if (param1 != 0) {
-              current = param2;
-            } else {
-              current += 3;
-            }
-            break;
-          case 6:
-            //jump to param2 if param1 is zero
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            std::cout << "param1: " << param1 << " param2 " << param2  << "\n";
-            if (param1 == 0) {
-              current = param2;
-            } else {
-              current += 3;
-            }
-            std::cout << "current: " << current << "\n";
-            break;
-          case 7:
-            //if param1 < param2, store 1 at param3
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            //param3 = mode.at(0) == '0' ? v[v[current + 3]] : v[current + 3];
-            std::cout << "param1: " << param1 << " param2 " << param2  << " param3 " << param3 << "\n";
-            if (param1 < param2) {
-              v[v[current + 3]] = 1;
-            } else {
-              v[v[current + 3]] = 0;
-            }
-            current += 4;
-            break;
-          case 8:
-            //if param1 equals param2, store 1 at param3
-            param1 = mode.at(2) == '0' ? v[v[current + 1]] : v[current + 1];
-            param2 = mode.at(1) == '0' ? v[v[current + 2]] : v[current + 2];
-            //param3 = mode.at(0) == '0' ? v[v[current + 3]] : v[current + 3];
-            std::cout << "param1: " << param1 << " param2 " << param2  << " param3 " << param3 << "\n";
-            if (param1 == param2) {
-              v[v[current + 3]] = 1;
-            } else {
-              v[v[current + 3]] = 0;
-            }
-            current += 4;
-            break;
-          case 99:
-            running = false;
-            break;
-          default:
-            std::cout << "Something went wrong, instruction: " << instruction << " mode: " << mode << "\n";
-            return 1;
-        }
-      }
+      result = create_and_run(arr[i], result, original);
     }
-    std::cout << "end of run " << input << "\n";
-    if (input > max) {
-      max = input;
+    std::cout << "end of run " << result << "\n";
+    if (result > max) {
+      max = result;
     }
   } while (std::next_permutation(arr, arr + 5));
   std::cout << "Part 1: " << max << "\n";
